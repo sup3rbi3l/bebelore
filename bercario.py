253d6e8a2445be7cf5ce5d725cc84474b655e6b5
sup3rbi3l <135216652+sup3rbi3l@users.noreply.github.com>
2023-11-26 22:37:11 -0300

    Add files via upload

--- bercario.py
+++ bercario.py
@@ -17,43 +17,47 @@ def main():
         escolha = sb.oqueFara()
         match escolha:
             case 1:
-                sb.mostraBebes()
+                escolha = bb.mostraBebe(bebe)
+                
+                
+                if (escolha == len(sistema.bebe.nome)):
+                    
+                    bb.criaBebe(sistema)
+                    
+                    
+                elif (escolha == len(sistema.bebe.nome)+1):
+                    pass
+                else:
+                    bb.mostra_informação(bebe,escolha) 
+         
+                
             case 2:
-                escolha = sb.mostraMae(sistema)
+                escolha = m.mostraMae(mae)
                     
                 if (escolha == len(sistema.mae.nome)):
                     nome,endereco,telefone,data = m.criaMae()
-                    mae.nome.append(nome)
-                    mae.endereco.append(endereco)
-                    mae.telefone.append(telefone)
-                    mae.data.append(data)
+                    m.appendmae(mae,nome,endereco,telefone,data)
+                    
+                    
                 elif (escolha == len(sistema.mae.nome)+1):
                     pass
                 else:
                     m.mostra_informação(mae,escolha)
             case 3: 
-                escolha = sb.mostraMedico(sistema)
+                escolha = me.mostraMedico(medico)
                     
                 if (escolha == len(sistema.medico.nome)):
-                    nome,telefone, crm,especialidade = me.criaMedico()
-                    medico.nome.append(nome)
-                    medico.telefone.append(telefone)
-                    medico.CRM.append(crm)
-                    medico.especialidade.append(especialidade)
+                    
+                    nome,telefone,crm,especialidade = me.criaMedico()
+                    me.appendMedico(medico,nome,telefone,crm,especialidade)
+                    
                 elif (escolha == len(sistema.medico.nome)+1):
                     pass
                 else:
                     me.mostra_informação(medico,escolha)
             case 4:
                 break
-
-        
-        
-
-
-        
-    
-    
+            
 if __name__ == '__main__':
     
     main()
--- sistema_bercario.py
+++ sistema_bercario.py
@@ -11,8 +11,6 @@ class SistemaBerco:
     def set_medico(self,medico):
         self.medico = medico
 
-
-
     def Onlyint(texto):
         valor = ""
         while type(valor) != int:
@@ -32,40 +30,9 @@ class SistemaBerco:
 
         return valor
 
-    def mostraBebes(self):
-        print(self.bebe.nome[0])
+  
     
-    def mostraMedico(self):
-        escolha = 0
-        print(len(self.medico.nome))
-        if len(self.medico.nome) == 0:
-            print('Não a nenhum medico no sistema')
-            
-        for i in range (len(self.medico.nome)):
-            print(f'{i+1}){self.medico.nome[i]}')
-        print(f'{len(self.medico.nome)+1})Adicionar medico')    
-        print(f'{len(self.medico.nome)+2})voltar')    
-        
-        while escolha <1 or escolha > len(self.medico.nome)+2:
-            escolha = SistemaBerco.Onlyint("==>")
-        escolha -=1
-        return escolha
     
-    def mostraMae(self):
-        escolha = 0
-        print(len(self.mae.nome))
-        if len(self.mae.nome) == 0:
-            print('Não a nenhuma mâe no sistema')
-            
-        for i in range (len(self.mae.nome)):
-            print(f'{i+1}){self.mae.nome[i]}')
-        print(f'{len(self.mae.nome)+1})Adicionar mae')    
-        print(f'{len(self.mae.nome)+2})voltar')    
-        
-        while escolha <1 or escolha > len(self.medico.nome)+2:
-            escolha = SistemaBerco.Onlyint("==>")
-        escolha -=1
-        return escolha
 
 
 class bebe:
@@ -76,9 +43,90 @@ class bebe:
         self.altura = altura
         self.mae = mae
         self.medico = medico
+        
+    def mostraBebe(self):
+        
+            escolha = 0
+            if len(self.nome) == 0:
+                print('Não a nenhum bebe no sistema')
+            
+            for i in range (len(self.nome)):
+                print(f'{i+1}){self.nome[i]}')
+            print(f'{len(self.nome)+1})Adicionar bebe:')    
+            print(f'{len(self.nome)+2})voltar')    
+            
+            while escolha <1 or escolha > len(self.nome)+2:
+                escolha = SistemaBerco.Onlyint("==>")
+            escolha -=1
+            return escolha
+        
+    def appendBebe(self,nome,data,peso,altura,mãe,medic):
+        self.nome.append(nome)
+        self.data.append(data)
+        self.peso.append(peso)
+        self.altura.append(altura)
+        self.mae.append(mãe)
+        self.medico.append(medic)
+        
+    def criaBebe(self):
+        escolha = 0
+        nome_bebe = input("digite o nome do bebe:")
+        data_bebe = input("digite a data de nascimento do bebe:")
+        peso = input("digite o peso do bebe:")
+        altura = input("digite a altura do bebe:")
+        
+        if len(self.mae.nome) == 0:
+            print('Não a nenhuma mãe no sistema, adicione ela')
+        else:
+            print('selecione a mãe do bebe,caso não esteve no sistema adicione ela')
+        for i in range (len(self.mae.nome)):
+            print(f'{i+1}){self.mae.nome[i]}')
+        print(f'{len(self.mae.nome)+1})Adicionar mãe:')    
+        
+        while escolha <1 or escolha > len(self.mae.nome)+1:
+            escolha = SistemaBerco.Onlyint("==>")
+        escolha -=1
+        if escolha == len(self.mae.nome):
+            nome,endereco,telefone,data = mae.criaMae()
+            mae.appendmae(self.mae,nome,endereco,telefone,data) 
+        
+        mãe = self.mae.nome[escolha]
+
+        escolha = 0
+        
+        
+        if len(self.medico.nome) == 0:
+            print('Não a nenhum medico no sistema, adicione o medico no sistema')
+        else:
+            ('selecione o medico do bebe,caso não esteve no sistema adicione o')
+        for i in range (len(self.medico.nome)):
+            print(f'{i+1}){self.medico.nome[i]}')
+        print(f'{len(self.medico.nome)+1})Adicionar medico')     
+        
+        while escolha <1 or escolha > len(self.medico.nome)+1:
+            escolha = SistemaBerco.Onlyint("==>")
+        escolha -=1
+        if (escolha == len(self.medico.nome)):
+                    
+            nome,telefone,crm,especialidade = medico.criaMedico()
+            medico.appendMedico(self.medico,nome,telefone,crm,especialidade)
+        medic = self.medico.nome[escolha]
+
+        bebe.appendBebe(self.bebe,nome_bebe,data_bebe,peso,altura,mãe,medic)
+
+    def mostra_informação(self,escolha):
+        print('nome:',self.nome[escolha])
+        print('data de nascimento:',self.data[escolha])
+        print('peso:',self.peso[escolha])
+        print('mãe:',self.mae[escolha])
+        print('medico:',self.medico[escolha])
+        
+        input('enter para continuar')
+
 
 
 class mae:
+    
     def __init__(self, nome, endereco, telefone, data):
         self.nome = nome
         self.endereco = endereco
@@ -93,6 +141,22 @@ class mae:
         
         input('enter para continuar')
         
+    def mostraMae(self):
+        escolha = 0
+        print(len(self.nome))
+        if len(self.nome) == 0:
+            print('Não a nenhuma mâe no sistema')
+            
+        for i in range (len(self.nome)):
+            print(f'{i+1}){self.nome[i]}')
+        print(f'{len(self.nome)+1})Adicionar mãe:')    
+        print(f'{len(self.nome)+2})voltar')    
+        
+        while escolha <1 or escolha > len(self.nome)+2:
+            escolha = SistemaBerco.Onlyint("==>")
+        escolha -=1
+        return escolha
+    
     def criaMae():
         nome = input("digite o nome da mãe:")
         endereco = input("digite o endereço da mãe:")
@@ -100,6 +164,12 @@ class mae:
         data = input("digite a data da mãe:")
 
         return nome, endereco, telefone, data
+
+    def appendmae(self,nome,endereco,telefone,data):
+        self.nome.append(nome)
+        self.endereco.append(endereco)
+        self.telefone.append(telefone)
+        self.data.append(data)
 
 class medico:
     def __init__(self, nome, telefone, CRM, especialidade):
@@ -122,4 +192,26 @@ class medico:
         especialidade = input("digite a especialidade do medico:")
 
         return nome, telefone, CRM, especialidade
+    
+    def appendMedico(self,nome, telefone, CRM, especialidade):
+        
+        self.nome.append(nome)
+        self.telefone.append(telefone)
+        self.CRM.append(CRM)
+        self.especialidade.append(especialidade)        
 
+    def mostraMedico(self):
+        escolha = 0
+        print(len(self.nome))
+        if len(self.nome) == 0:
+            print('Não a nenhum medico no sistema')
+            
+        for i in range (len(self.nome)):
+            print(f'{i+1}){self.nome[i]}')
+        print(f'{len(self.nome)+1})Adicionar medico')    
+        print(f'{len(self.nome)+2})voltar')    
+        
+        while escolha <1 or escolha > len(self.nome)+2:
+            escolha = SistemaBerco.Onlyint("==>")
+        escolha -=1
+        return escolha  
